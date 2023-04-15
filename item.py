53fe479328b13a84cfbd6de9d7f608dbee9f47bd
vamshi5421 <75656830+vamshi5421@users.noreply.github.com>
2023-04-15 21:48:57 +0530

    items, phones

---
+++ item.py
@@ -0,0 +1,56 @@
+import csv
+
+class Item:
+
+    pay_rate = 0.8 
+    all = [] 
+
+
+    def __init__(self, name: str, price: float, quantity = 0): 
+        
+        # Run Validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than or equal to zero"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal zero"
+
+        # Assign to self object
+        self.name = name
+        self.price = price 
+        self.quantity = quantity
+
+        # Add the object to the list
+        Item.all.append(self) 
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+    
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open("items.csv") as file:
+            reader = csv.DictReader(file)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name = item["name"],
+                price = float(item["price"]),
+                quantity = int(item["quantity"])
+            )
+
+    @staticmethod
+    def is_integer(num):
+        if isinstance(num, float):
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else :
+            return False
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name},' {self.price}, {self.quantity})"
+
+
+Item.instantiate_from_csv()
---
+++ phone.py
@@ -0,0 +1,12 @@
+class Phone(Item):
+    
+
+    def __init__(self, name: str, price: float, quantity = 0, broken_phone = 0): 
+        
+        super().__init__(name, price, quantity)
+
+        # Run Validations to the received arguments
+        assert broken_phone >= 0, f"Broken Phones {broken_phone} is not greater than or equal zero"
+        
+        # Assign to self object
+        self.broken_phone = broken_phone
